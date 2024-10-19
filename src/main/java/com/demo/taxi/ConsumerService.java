7d04ef86d766d8a7c4326aa4435ff1abcb160551
dsn777 <dsn300703@mail.ru>
2024-10-19 18:17:29 +0300

    first commit

---
+++ src/main/java/com/demo/taxi/ConsumerService.java
@@ -0,0 +1,14 @@
+package com.demo.taxi;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void listen(String message) {
+        System.out.println("Recieved: " + message);
+    }
+}
+
---
+++ src/main/java/com/demo/taxi/MessageController.java
@@ -0,0 +1,20 @@
+package com.demo.taxi;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    private final ProducerService producerService;
+
+    public MessageController(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestParam String message) {
+        producerService.sendMessage(message);
+    }
+}
