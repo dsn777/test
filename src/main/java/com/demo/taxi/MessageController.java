28f6141627c81643368b550fad2fe3124613e57d
dsn777 <dsn300703@mail.ru>
2024-11-01 13:26:49 +0300

    last commit

--- src/main/java/com/demo/taxi/MessageController.java
+++ src/main/java/com/demo/taxi/MessageController.java
@@ -1,19 +1,20 @@
-package com.demo.taxi;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MessageController {
-    private final ProducerService producerService;
-
-    public MessageController(ProducerService producerService) {
-        this.producerService = producerService;
-    }
-
-    @PostMapping("/send")
-    public void sendMessage(@RequestParam String message) {
-        producerService.sendMessage(message);
-    }
-}
+//package com.demo.taxi;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class MessageController {
+//    private final ProducerService producerService;
+//
+//    public MessageController(ProducerService producerService) {
+//        this.producerService = producerService;
+//    }
+//
+//    @GetMapping("/send")
+//    public void sendMessage(@RequestParam String message) {
+//        producerService.sendMessage(message);
+//    }
+//}
--- src/main/java/com/demo/taxi/TaxiApplication.java
+++ src/main/java/com/demo/taxi/TaxiApplication.java
@@ -1,12 +1,66 @@
 package com.demo.taxi;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class TaxiApplication {
+
+	@Autowired
+	public TaxiApplication(ConsumerService consumerService, ProducerService producerService) {
+		this.consumerService = consumerService;
+		this.producerService = producerService;
+	}
+
+	public ConsumerService consumerService;
+	public static ProducerService producerService;
+
 	public static void main(String[] args) {
-		SpringApplication.run(TaxiApplication.class, args);
-		System.out.println();
+		SpringApplication.run(TaxiApplication.class);
+		// Параметры Kafka
+		String bootstrapServers = "192.168.0.197:9092";
+		String topic = "my-topic";
+
+		// Конфигурация производителя
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+
+		// Запуск 100 клиентов в отдельных потоках
+		for (int i = 0; i < 1000; i++) {
+			int finalI = i;
+			new Thread(() -> {
+				// Создание производителя
+				KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+				while (true) {
+					// Создание сообщения
+					String message = "Сообщение от клиента " + (finalI + 1);
+
+					// Отправка сообщения
+					producer.send(new ProducerRecord<>(topic, message));
+
+					// Пауза на 1 секунду
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}).start();
+		}
+
+		// Закрытие производителя
+		//producer.close();
 	}
 }
+
